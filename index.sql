33a45f64a79867b362daa8d55d0eb6777c2705b9
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-30 22:42:51 +0200

    es 5 sone

--- index.sql
+++ index.sql
@@ -11,7 +11,7 @@ SELECT * FROM students WHERE YEAR(CURDATE()) - YEAR(date_of_birth) > 30;
 SELECT * FROM courses WHERE period = 'I semestre' AND year = 1;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * FROM exams WHERE DATE(date) = '2020-06-20' AND TIME(hour) > '14:00';
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
 
