5b46d15b97155979564fc59e00c7bb5c95badd3c
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-30 22:41:25 +0200

    es 4 done

--- index.sql
+++ index.sql
@@ -8,7 +8,7 @@ SELECT * FROM courses WHERE cfu > 10;
 SELECT * FROM students WHERE YEAR(CURDATE()) - YEAR(date_of_birth) > 30;
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * FROM courses WHERE period = 'I semestre' AND year = 1;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
@@ -22,13 +22,13 @@ SELECT * FROM students WHERE YEAR(CURDATE()) - YEAR(date_of_birth) > 30;
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 
---1. Contare quanti iscritti ci sono stati ogni anno
+--9. Contare quanti iscritti ci sono stati ogni anno
 
 
---2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+--10. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
 
---3. Calcolare la media dei voti di ogni appello d'esame
+--11. Calcolare la media dei voti di ogni appello d'esame
 
 
---4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+--12. Contare quanti corsi di laurea ci sono per ogni dipartimento
